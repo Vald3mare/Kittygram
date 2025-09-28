aedce3b0a37804bf1ac37670b78777e5c92975a3
emelyanovvv@navigator.private <->
2025-09-28 20:06:03 +0300

    minor

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,11 +1,14 @@
-terraform{
-    backend "s3" {
-      endpoint = "https://storage.yandexcloud.net"
-      bucket = "vald3mar-kittygram-tf"
-      region = "ru-central1"
-      key = "terraform.tfstate"
-
-      skip_region_validation      = true
-      skip_credentials_validation = true
+terraform {
+  backend "s3" {
+    endpoints = {
+      s3 = "https://storage.yandexcloud.net"
     }
+    
+    bucket = "vald3mar-kittygram-tf"
+    region = "ru-central1"
+    key    = "terraform.tfstate"
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+  }
 }

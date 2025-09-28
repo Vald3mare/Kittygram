c3f017e6cc133ff4e77de6d012bb6f7bd08da1d6
emelyanovvv@navigator.private <->
2025-09-28 23:41:31 +0300

    terraform refactor cloud-init

--- infra/main.tf
+++ infra/main.tf
@@ -9,7 +9,6 @@ terraform {
 
 provider "yandex" {
   service_account_key_file = var.yc_token
-  #token                    = var.yc_token
   cloud_id                 = var.cloud_id
   folder_id                = var.folder_id
   zone                     = "ru-central1-b"
